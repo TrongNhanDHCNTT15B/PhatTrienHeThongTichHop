07bc2adb7de9379e73a634e968b98b8410cf0468
TrongNhanDHCNTT15B <90305223+TrongNhanDHCNTT15B@users.noreply.github.com>
2021-11-03 21:24:44 +0700

    Update RMIClient.java

--- RMIClient.java
+++ RMIClient.java
@@ -1,67 +1,60 @@
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.util.Scanner;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 public class RMIClient {
-private static final String HOST = "localhost";
-private static final int PORT = 2109;
-private static Registry registry;
 
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
 
         try {
-            registry = LocateRegistry.getRegistry(HOST, PORT);
             Adder a = (Adder) Naming.lookup("rmi://localhost/adder");
             int select;
             Scanner scanner = new Scanner(System.in);
             for (;;) {
                 System.out.println("----------MENU----------");
-                System.out.println("1. Đếm số lượng từ");
-                System.out.println("2. Đảo chuỗi");
-                System.out.println("Nhập exit trong chức năng đang chọn để thoát");
-
+                System.out.println("1. Dem");
+                System.out.println("2. Dao chuoi");
+                System.out.println("Nhap exit trong chuc nang bat ki de thoat");
                 Scanner sccc = new Scanner(System.in);
-                System.out.print("Chọn chức năng: ");
+                System.out.print("Chon chuc nang: ");
                 select = sccc.nextInt();
                 switch (select) {
                     case 1:
                         String v;
                         Scanner sc = new Scanner(System.in);
                         System.out.println("----------------------");
-                        System.out.println("Chức năng 1 đang chạy");
-                        System.out.print("Nhập vào chuỗi: ");
+                        System.out.println("Chuc nang 1 dang chay");
+                        System.out.print("Nhap vao chuoi: ");
                         v = sc.nextLine();
                         String ex = "exit";
                         if(v.equals(ex)==true){
-                            System.out.println("Bạn chọn thoát chương trình. Tạm biệt!");
+                            System.out.println("Ban chon thoat");
                             System.exit(0);
                         } else {
                             int dem = a.count(v);
-                            System.out.println("Chuỗi đã nhập: " + v);
-                            System.out.println("Số lượng từ trong chuỗi: " + dem);
-                            }
+                            System.out.println("Chuoi da nhap: " + v);
+                            System.out.println("So luong tu trong chuoi: " + dem);
+                        }
                         break;
                     case 2:
                         String k;
                         Scanner scc = new Scanner(System.in);
                         System.out.println("----------------------");
-                        System.out.println("Chức năng 2 đang chạy");
-                        System.out.print("Nhập vào chuỗi: ");
+                        System.out.println("Chuc nang 2 dang chay");
+                        System.out.print("Nhap vao chuoi: ");
                         k = scc.nextLine();
                         String exx = "exit";
                         if(k.equals(exx)==true){
-                            System.out.println("Bạn chọn thoát chương trình. Tạm biệt!");
+                            System.out.println("Ban chon thoat");
                             System.exit(0);
                         } else {
                             String p = a.daonguoc(k);
-                            System.out.println("Chuỗi sau khi đảo: " + p);
-                            }
+                            System.out.println("Chuoi sau khi dao: " + p);
+                        }
                         break;
                     default:
-                        System.out.println("Không có chức năng tương ứng. Vui lòng nhập lại");
+                        System.out.println("Khong co chuc nang tuong ung. Nhap lai");
                         break;
                 }
             }
