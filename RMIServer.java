63658096f3a24366e6b5c3eecf8f5075f35effc4
TrongNhanDHCNTT15B <90305223+TrongNhanDHCNTT15B@users.noreply.github.com>
2021-11-03 21:25:25 +0700

    Update RMIServer.java

--- RMIServer.java
+++ RMIServer.java
@@ -1,21 +1,14 @@
 import java.net.MalformedURLException;
 import java.rmi.*;
-import java.io.*;
-import java.net.Socket;
-import java.net.ServerSocket;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+
 
 public class RMIServer {
-    private static final int PORT = 2109;
-    private static Registry registry;
-    public static void main(String args[]) throws RemoteException {
+    public static void main(String args[]) {
 
         try {
-            registry = LocateRegistry.createRegistry(PORT);
             AdderImpl adder = new AdderImpl();
             Naming.rebind("adder", adder);
-            System.out.println("Server đang chạy");
+            System.out.println("Server dang chay");
         } catch (RemoteException re) {
             re.printStackTrace();
         } catch (MalformedURLException me) {
